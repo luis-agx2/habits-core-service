20110e75236629f654e513cfb2dfc08b4493c340
Luis Alejandor G <luisalejandrogarciax2@gmail.com>
2026-06-27 21:58:27 -0600

    init project

---
+++ src/main/java/com/habits/platform/habits/HabitsApplication.java
@@ -0,0 +1,13 @@
+package com.habits.platform.habits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitsApplication.class, args);
+    }
+
+}
